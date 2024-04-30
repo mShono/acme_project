80381fe31f1ae437c8460003b9fca19ee1012e75
Mariia Samokhina <mshono@yandex.ru>
2024-04-30 13:56:10 +0300

    Работа с формами 5. Формы на основе моделей

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import BirthdayForm
+
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = BirthdayForm
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
