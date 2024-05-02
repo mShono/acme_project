62c1e1a24ff31dcb45ee5b32d04f94f206920611
Mariia Samokhina <mshono@yandex.ru>
2024-05-02 11:40:31 +0300

    Работа с формами 7. Создание записей в БД через форму.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
+from .models import BirthdayForm
 
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    birthdays = BirthdayForm.objects.all()
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
