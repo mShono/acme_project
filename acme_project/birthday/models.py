8b69fe73f5c677e3405758ae7f1c62f1e9a7e61f
Mariia Samokhina <mshono@yandex.ru>
2024-05-06 13:15:11 +0300

    Работа с формами 10. Работа с изображениями в формах

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
@@ -8,4 +10,14 @@ class Birthday(models.Model):
     )
     birthday = models.DateField(
         'Дата рождения',
+        validators=(real_age,)
     )
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
