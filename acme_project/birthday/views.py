0bee268c98bb73e7ca684c9cd7234ff8bb8ea06e
Mariia Samokhina <mshono@yandex.ru>
2024-05-07 14:25:06 +0300

    Представления: расширенные возможности 3. класс DetailView

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,49 +1,40 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-class BirthdayMixin:
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
+
+
+class BirthdayCreateView(CreateView):
+    """Класс, отвечающий за создание формы"""
     model = Birthday
     form_class = BirthdayForm
-    success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayCreateView(BirthdayMixin, CreateView):
-    """Класс, отвечающий за создание формы"""
-    pass
-
-
-# До создания этого класса его функции были описаны в функции birthday
-class BirthdayUpdateView(BirthdayMixin, UpdateView):
+class BirthdayUpdateView(UpdateView):
     """Класс, отвечающий за редактирование"""
-    pass
-
-
-# Функция, работавшая до создания класса BirthdayCreateView:
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+    model = Birthday
+    form_class = BirthdayForm
 
 
 class BirthdayListView(ListView):
@@ -51,35 +42,7 @@ class BirthdayListView(ListView):
     ordering = 'id'
     paginate_by = 3
 
-# Функция, работавшая до создания класса BirthdayListView:
-def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
-    birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
-    paginator = Paginator(birthdays, 2)
-    # Получаем из запроса значение параметра page.
-    page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора.
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
-    page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст
-    # объект страницы пагинатора
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
-
 
 class BirthdayDeleteView(DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
-
-
-# Функция, работавшая до создания класса BirthdayDeleteView:
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
