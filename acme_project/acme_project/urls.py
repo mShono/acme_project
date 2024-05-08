c604c5f45988752974f378c083c8f27511543fa7
Mariia Samokhina <mshono@yandex.ru>
2024-05-08 17:25:16 +0300

    10. Пользователи в Django 5. Настройка страницы регистрации

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -3,10 +3,21 @@ from django.conf import settings
 
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
