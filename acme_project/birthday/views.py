3f6a75d507b3a292bafb8ca4b1a66d45566ad71f
Mariia Samokhina <mshono@yandex.ru>
2024-05-10 15:44:35 +0300

    10. Пользователи в Django 6. Разграничение доступа

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,7 @@
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -10,39 +12,60 @@ from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+    """Миксин для тестирования пользователей, обращающихся к объекту."""
+
+    def test_func(self):
+        # Получаем текущий объект.
+        object = self.get_object()
+        # Метод вернёт True или False.
+        # Если пользователь - автор объекта, то тест будет пройден.
+        # Если нет, то будет вызвана ошибка 403.
+        return object.author == self.request.user
+
+
 class BirthdayDetailView(DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
-        # Получаем словарь контекста:
         context = super().get_context_data(**kwargs)
-        # Добавляем в словарь новый ключ:
         context['birthday_countdown'] = calculate_birthday_countdown(
-            # Дату рождения берём из объекта в словаре context:
             self.object.birthday
         )
-        # Возвращаем словарь контекста.
         return context
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     """Класс, отвечающий за создание формы"""
     model = Birthday
     form_class = BirthdayForm
 
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form) 
 
-class BirthdayUpdateView(UpdateView):
+
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     """Класс, отвечающий за редактирование"""
     model = Birthday
     form_class = BirthdayForm
 
 
 class BirthdayListView(ListView):
+    """Класс, отвечающий за количество объектов в списке на странице"""
     model = Birthday
     ordering = 'id'
     paginate_by = 3
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
+    """Класс, отвечающий за удаление"""
     model = Birthday
     success_url = reverse_lazy('birthday:list')
